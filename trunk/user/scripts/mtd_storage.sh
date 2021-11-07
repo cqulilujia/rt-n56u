c02c3378c1f0c3697ec5b8c4442a2832195848ea
Lucas <33367939+cqulilujia@users.noreply.github.com>
2021-11-07 15:48:21 +0800

    Update mtd_storage.sh

--- trunk/user/scripts/mtd_storage.sh
+++ trunk/user/scripts/mtd_storage.sh
@@ -322,7 +322,7 @@ EOF
 ### \$1 - WAN action (up/down)
 ### \$2 - WAN interface name (e.g. eth3 or ppp0)
 ### \$3 - WAN IPv4 address
-if [ \$1 == "up" ] ; then
+if [ $1 == "up" ] ; then
     ### drcom
     sleep 10 && dogcom -m dhcp -c /etc/storage/drcom.conf -e -d &
 fi
